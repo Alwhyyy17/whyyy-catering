d9048e33332ac30451adb32ba1ddcb36da588058
Alwhyyy <alwiubaidillah1@gmail.com>
2025-07-01 04:03:01 +0700

    U1pdate

--- app.py
+++ app.py
@@ -1262,4 +1262,4 @@ def hapus_pesanan_kadaluarsa():
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port)
